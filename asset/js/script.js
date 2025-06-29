9a60397b5947fee3ebe43a919d0a094b8c7b2995
Michele del Giudice <dgmichele96@gmail.com>
2025-06-29 19:13:25 +0200

    snellito codice

--- asset/js/script.js
+++ asset/js/script.js
@@ -56,52 +56,5 @@ function calcolaEmissioniTotali(consumoEnergetico, chilometriPercorsi) {
         }
 
         // Aggiorniamo il contenuto dello span con il risultato
-        spanRisultato.innerText = `${emissioniTotali.toFixed(2)} kg di CO2`;
-
-         // Creiamo un nuovo paragrafo in cui stampare il banner di valutazione
-        let nuovoParagrafo = document.getElementById('risultato')
-
-        // Controlliamo se il paragrafo esiste già, se non esiste, lo creiamo
-        if(!nuovoParagrafo) {
-            let nuovoParagrafo = document.createElement('p');
-            nuovoParagrafo.id = 'risultato';
-            // Individuiamo il contenitore e aggiungiamo il nuovo <p>
-            let testoRisultato = document.querySelector('.interno-calcolatore');
-            testoRisultato.append(nuovoParagrafo);
-        }
-
-        if(emissioniTotali < 50){
-            // Mostriamo il risultato a schermo
-            document.getElementById('risultato').innerText = "✅ Sei un top cittadino!";
-
-            // Cerchiamo il banner (l'elemento con id 'risultato')
-            let banner = document.getElementById('risultato');
-            
-            // Otteniamo la posizione del banner rispetto alla finestra del browser
-            let posizioneBanner = banner.getBoundingClientRect();
-            
-            // Calcoliamo l'origine dei coriandoli in base alla posizione del banner
-            // (posizioneBanner.left + posizioneBanner.right) / 2 restituisce il centro del banner in termini di larghezza
-            // (posizioneBanner.top + posizioneBanner.bottom) / 2 restituisce il centro del banner in termini di larghezza
-            // i risultati delle operazioni sopra, divise per document.documentElement.client... , restituiscono un valore tra 0 e 1, utile per assegnare le coordinate alla funzione "confetti"
-            let bannerX = (posizioneBanner.left + posizioneBanner.right) / 2 / document.documentElement.clientWidth;  // Coordinata orizzontale
-            let bannerY = (posizioneBanner.top + posizioneBanner.bottom) / 2 / document.documentElement.clientHeight; // Coordinata verticale
-
-            // Lancia i coriandoli dall'area del banner
-            confetti({
-                particleCount: 100,
-                spread: 70,
-                origin: { x: bannerX, y: bannerY }
-            });
-
-        } else {
-            // Se si supera 50 mostriamo un banner rosso per indicare il risultato negativo
-            document.getElementById('risultato').innerText = "❌ Ma quanto inquini?!";
-            document.getElementById('risultato').style.color = 'red';
-            document.getElementById('risultato').style.backgroundColor = '#fffef2';
-            document.getElementById('risultato').style.padding = '20px 30px';
-            document.getElementById('risultato').style.borderRadius = '7px';
-            document.getElementById('risultato').style.border = '2px solid red';
-            document.getElementById('risultato').style.borderLeft = '7px solid red';
-        }
+        spanRisultato.innerText = `${emissioniTotali.toFixed(2)} kg di CO₂`;
     });
